360697e49ccb2916249bcbafe7c7652dc3bad7c9
Robert McCarter <69922179+robertmc5@users.noreply.github.com>
2022-09-19 23:44:18 -0400

    Structure nameGuess #games space
    And enter Emmy names data

---
+++ resources/pages/nameGuess.js
@@ -0,0 +1,14 @@
+const emmyNames1 = [
+  'Emmycat', 'Kitten', 'Bubby Cubs', 'Fluff', 'Schmoopy', 
+  'Smitten Kitten', 'Fuzzy Butt', 'Princess Ponytrot', 'Baby Fat', 'Squeakers'
+];
+const emmyNames2 = [
+  'Little Miss Kitten', 'Fuzzy Button', 'Kitty Mitty', 'Scoochie Booch', 'Marshmallow Fluff', 
+  'Schmubby Bub', 'Baby Fluff', 'The 19-Toed One', 'Bubbyduders', 'Baby Girl'
+];
+const emmyNames3 = [
+  'Kittenduders', 'Squeak Toy Joy', 'Kitten Mitten', 'Little Bit', 'Scoochie Bear', 
+  'Fatticus Catticus', 'Bubbie Schmubs', 'Fluffy Muff', 'Baby Kitten', 'Squeaky Reminder'
+];
+
+let randomGroup = Math.floor(Math.random() * 3);
