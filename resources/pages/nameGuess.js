f78d08da8e7751bd7d0b6602f814681e0ccda019
Robert McCarter <69922179+robertmc5@users.noreply.github.com>
2022-09-20 23:21:27 -0400

    Develop nameGuess
    Pick and display names

--- resources/pages/nameGuess.js
+++ resources/pages/nameGuess.js
@@ -1,3 +1,4 @@
+// Three sets of names to pick from
 const emmyNames1 = [
   'Emmycat', 'Kitten', 'Bubby Cubs', 'Fluff', 'Schmoopy', 
   'Smitten Kitten', 'Fuzzy Butt', 'Princess Ponytrot', 'Baby Fat', 'Squeakers'
@@ -11,4 +12,55 @@ const emmyNames3 = [
   'Fatticus Catticus', 'Bubbie Schmubs', 'Fluffy Muff', 'Baby Kitten', 'Squeaky Reminder'
 ];
 
-let randomGroup = Math.floor(Math.random() * 3);
+// Helper function chooses the set of names used for this game
+let chosenGroup = [];
+const nameGroup = () => {
+  let randomGroup = Math.floor(Math.random() * 3);
+  switch (randomGroup) {
+    case 0: return emmyNames1;
+    case 1: return emmyNames2;
+    case 2: return emmyNames3;
+  }
+}
+
+// Function chooses the name to guess for this game and assigns the name group
+const pickNameAndGroup = () => {
+  chosenGroup = nameGroup();
+  let randomName = Math.floor(Math.random() * 10);
+  let chosenName = chosenGroup[randomName];
+  return chosenName;
+}
+
+// Function for game play
+const playGame = () => {
+  counter = 0;                          /* TODO */
+  let winner = pickNameAndGroup();
+  gameActive = true;
+  let showPlace = document.getElementsByClassName('names-display')[0];
+  let randomName;
+  let showAName;
+  while (gameActive) {
+    randomName = Math.floor(Math.random() * 10);
+    showAName = document.createElement('p');
+    showAName.innerHTML = chosenGroup[randomName];
+
+    showAName.style.fontSize = '0.75rem';
+    showAName.style.position = 'absolute';
+    showAName.style.bottom = '3%';
+    showAName.style.left = '3%';
+    showPlace.appendChild(showAName);
+    showAName.style.transition = 'all 1.5s';
+    setTimeout(() => {
+      showAName.style.fontSize = '1.25rem';
+      showAName.style.bottom = '80%';
+      showAName.style.left = '67%';
+    }, 10)
+    // showPlace.removeChild(showPlace.firstElementChild);
+    counter ++;                          /* TODO */
+    if (counter === 7) {             /* TODO */
+      gameActive = false;
+    }
+  }
+}
+
+playGame();
