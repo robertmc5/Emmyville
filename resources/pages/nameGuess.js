9910e12e3117240a40e624fabb9a14bc3d6ff898
Robert McCarter <69922179+robertmc5@users.noreply.github.com>
2022-09-21 03:11:50 -0400

    Experiment with movement of name options

--- resources/pages/nameGuess.js
+++ resources/pages/nameGuess.js
@@ -1,63 +1,89 @@
 // Three sets of names to pick from
 const emmyNames1 = [
-  'Emmycat', 'Kitten', 'Bubby Cubs', 'Fluff', 'Schmoopy', 
-  'Smitten Kitten', 'Fuzzy Butt', 'Princess Ponytrot', 'Baby Fat', 'Squeakers'
+  'Little M1ss Kitten', 'Little M2ss Kitten', 'Little M3ss Kitten', 'Little M4ss Kitten', 'Little M5ss Kitten', 'Little M26s Kitten'
 ];
 const emmyNames2 = [
-  'Little Miss Kitten', 'Fuzzy Button', 'Kitty Mitty', 'Scoochie Booch', 'Marshmallow Fluff', 
-  'Schmubby Bub', 'Baby Fluff', 'The 19-Toed One', 'Bubbyduders', 'Baby Girl'
+  'Little M6ss Kitten', 'Little M7ss Kitten', 'Little M8ss Kitten', 'Little M9ss Kitten', 'Little M10s Kitten', 'Little M27s Kitten'
 ];
 const emmyNames3 = [
-  'Kittenduders', 'Squeak Toy Joy', 'Kitten Mitten', 'Little Bit', 'Scoochie Bear', 
-  'Fatticus Catticus', 'Bubbie Schmubs', 'Fluffy Muff', 'Baby Kitten', 'Squeaky Reminder'
+  'Little M11s Kitten', 'Little M12s Kitten', 'Little M13s Kitten', 'Little M14s Kitten', 'Little M15s Kitten', 'Little M28s Kitten'
+];
+const emmyNames4 = [
+  'Little M16s Kitten', 'Little M17s Kitten', 'Little M18s Kitten', 'Little M19s Kitten', 'Little M20s Kitten', 'Little M29s Kitten'
+];
+const emmyNames5 = [
+  'Little M21s Kitten', 'Little M22s Kitten', 'Little M23s Kitten', 'Little M24s Kitten', 'Little M25s Kitten', 'Little M30s Kitten'
 ];
 
 // Helper function chooses the set of names used for this game
 let chosenGroup = [];
 const nameGroup = () => {
-  let randomGroup = Math.floor(Math.random() * 3);
+  let randomGroup = Math.floor(Math.random() * 5);
   switch (randomGroup) {
     case 0: return emmyNames1;
     case 1: return emmyNames2;
     case 2: return emmyNames3;
+    case 3: return emmyNames4;
+    case 4: return emmyNames5;
   }
 }
 
 // Function chooses the name to guess for this game and assigns the name group
 const pickNameAndGroup = () => {
   chosenGroup = nameGroup();
-  let randomName = Math.floor(Math.random() * 10);
+  let randomName = Math.floor(Math.random() * 6);
   let chosenName = chosenGroup[randomName];
   return chosenName;
 }
 
 // Function for game play
-const playGame = () => {
+function delay(ms) {
+  return new Promise (resolve => {
+    setTimeout(() => {resolve('')}, ms);
+  })
+}
+async function playGame() {
   counter = 0;                          /* TODO */
   let winner = pickNameAndGroup();
   gameActive = true;
   let showPlace = document.getElementsByClassName('names-display')[0];
   let randomName;
   let showAName;
+  let displayedGroup = [];
+  let picked = false;
+  let bottom = false;
   while (gameActive) {
-    randomName = Math.floor(Math.random() * 10);
     showAName = document.createElement('p');
-    showAName.innerHTML = chosenGroup[randomName];
 
-    showAName.style.fontSize = '0.75rem';
+    while (!picked) {
+      randomName = Math.floor(Math.random() * 6);
+      if (!displayedGroup.some(name => name === chosenGroup[randomName])) {
+        showAName.innerHTML = chosenGroup[randomName];
+        displayedGroup.push(chosenGroup[randomName]);
+        picked = true;
+        bottom ? bottom = false : bottom = true;
+        if (displayedGroup.length === 6) {
+          displayedGroup = [];
+        }
+      }
+    }
+    picked = false;
+
+    showAName.style.fontSize = '0.5rem';
     showAName.style.position = 'absolute';
-    showAName.style.bottom = '3%';
-    showAName.style.left = '3%';
+    bottom ? showAName.style.bottom = '1%' : showAName.style.bottom = '95%';
+    showAName.style.right = '10%';
     showPlace.appendChild(showAName);
     showAName.style.transition = 'all 1.5s';
     setTimeout(() => {
       showAName.style.fontSize = '1.25rem';
-      showAName.style.bottom = '80%';
-      showAName.style.left = '67%';
-    }, 10)
-    // showPlace.removeChild(showPlace.firstElementChild);
+      bottom ? showAName.style.bottom = '95%' : showAName.style.bottom = '1%';
+      showAName.style.right = '10%';
+    }, 20)
+    await delay(1180);
+    showPlace.removeChild(showPlace.firstElementChild);
     counter ++;                          /* TODO */
-    if (counter === 7) {             /* TODO */
+    if (counter === 12) {             /* TODO */
       gameActive = false;
     }
   }
