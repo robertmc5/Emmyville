ab66a3df177c9cfdcc578377ebabfb446412f0ee
Robert McCarter <69922179+robertmc5@users.noreply.github.com>
2022-09-22 23:33:00 -0400

    Finish moving display of name options
    Movement direction changes with mobile

--- resources/pages/nameGuess.js
+++ resources/pages/nameGuess.js
@@ -1,18 +1,18 @@
 // Three sets of names to pick from
 const emmyNames1 = [
-  'Little M1ss Kitten', 'Little M2ss Kitten', 'Little M3ss Kitten', 'Little M4ss Kitten', 'Little M5ss Kitten', 'Little M26s Kitten'
+  'Baby Kitten', 'Bubby Cubs', 'Schmoopy', 'Fuzzy Butt', 'Princess Ponytrot', 'Kitty Mitty'
 ];
 const emmyNames2 = [
-  'Little M6ss Kitten', 'Little M7ss Kitten', 'Little M8ss Kitten', 'Little M9ss Kitten', 'Little M10s Kitten', 'Little M27s Kitten'
+  'Little Miss Kitten', 'Fuzzy Button', 'Scoochie Booch', 'Schmubby Bub', 'Bubbyduders', 'Baby Fluff'
 ];
 const emmyNames3 = [
-  'Little M11s Kitten', 'Little M12s Kitten', 'Little M13s Kitten', 'Little M14s Kitten', 'Little M15s Kitten', 'Little M28s Kitten'
+  'Kittenduders', 'Little Bit', 'Scoochie Bear', 'Kitten', 'Fluffy Muff', 'Squeaky Reminder'
 ];
 const emmyNames4 = [
-  'Little M16s Kitten', 'Little M17s Kitten', 'Little M18s Kitten', 'Little M19s Kitten', 'Little M20s Kitten', 'Little M29s Kitten'
+  'Smitten Kitten', 'Baby Fat', 'Marshmallow Fluff', 'Squeak Toy Joy', 'The 19-Toed One', 'Emmycat'
 ];
 const emmyNames5 = [
-  'Little M21s Kitten', 'Little M22s Kitten', 'Little M23s Kitten', 'Little M24s Kitten', 'Little M25s Kitten', 'Little M30s Kitten'
+  'Squeakers', 'Kitten Mitten', 'Bubbie Schmubs', 'Fatticus Catticus', 'Baby Girl', 'Fluff'
 ];
 
 // Helper function chooses the set of names used for this game
@@ -36,25 +36,40 @@ const pickNameAndGroup = () => {
   return chosenName;
 }
 
-// Function for game play
+// Helper function for delay in game play
 function delay(ms) {
   return new Promise (resolve => {
     setTimeout(() => {resolve('')}, ms);
   })
 }
+
+// Helper function for display of names relative to viewport width
+function displayDirection(e) {
+  if (e.matches) {
+    verticle = true;
+  }
+  else {
+    verticle = false;
+  }
+}
+
+// Function to play game
 async function playGame() {
   counter = 0;                          /* TODO */
   let winner = pickNameAndGroup();
-  gameActive = true;
-  let showPlace = document.getElementsByClassName('names-display')[0];
+  let gameActive = true;
+  let showPlace = document.getElementById('games');
   let randomName;
   let showAName;
   let displayedGroup = [];
   let picked = false;
   let bottom = false;
+  let right = false;
+  const mM = window.matchMedia("(min-width: 800px)");
+  displayDirection(mM);
+  mM.addEventListener('change', displayDirection);
   while (gameActive) {
     showAName = document.createElement('p');
-
     while (!picked) {
       randomName = Math.floor(Math.random() * 6);
       if (!displayedGroup.some(name => name === chosenGroup[randomName])) {
@@ -62,28 +77,40 @@ async function playGame() {
         displayedGroup.push(chosenGroup[randomName]);
         picked = true;
         bottom ? bottom = false : bottom = true;
+        right ? right = false : right = true;
         if (displayedGroup.length === 6) {
           displayedGroup = [];
         }
       }
     }
     picked = false;
-
     showAName.style.fontSize = '0.5rem';
     showAName.style.position = 'absolute';
-    bottom ? showAName.style.bottom = '1%' : showAName.style.bottom = '95%';
-    showAName.style.right = '10%';
-    showPlace.appendChild(showAName);
-    showAName.style.transition = 'all 1.5s';
-    setTimeout(() => {
-      showAName.style.fontSize = '1.25rem';
-      bottom ? showAName.style.bottom = '95%' : showAName.style.bottom = '1%';
+    showAName.style.transform = 'rotate(-180deg)';
+    if (verticle) {
+      bottom ? showAName.style.bottom = '10%' : showAName.style.bottom = '95%';
       showAName.style.right = '10%';
+    }
+    if (!verticle) {
+      showAName.style.bottom = '83%';
+      right ? showAName.style.right = '4%' : showAName.style.right = '75%';
+    }
+    showPlace.appendChild(showAName);
+    showAName.style.transition = 'all 1.5s ease';
+    setTimeout(() => {
+      showAName.style.fontSize = '1.5rem';
+      if (verticle) {
+        bottom ? showAName.style.bottom = '95%' : showAName.style.bottom = '7%';
+      }
+      if (!verticle) {
+        right ? showAName.style.right = '40%' : showAName.style.right = '2%';
+      }
+      showAName.style.transform = 'rotate(5deg)';
     }, 20)
     await delay(1180);
-    showPlace.removeChild(showPlace.firstElementChild);
+    showPlace.removeChild(showPlace.lastElementChild);
     counter ++;                          /* TODO */
-    if (counter === 12) {             /* TODO */
+    if (counter === 30) {             /* TODO */
       gameActive = false;
     }
   }
