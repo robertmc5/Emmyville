d05d2ce5b8789ce4430b06c5097ef82e61580fc1
Robert McCarter <69922179+robertmc5@users.noreply.github.com>
2022-09-25 18:35:20 -0400

    Finish nameGuess game for now

--- resources/pages/nameGuess.js
+++ resources/pages/nameGuess.js
@@ -9,7 +9,7 @@ const emmyNames3 = [
   'Kittenduders', 'Little Bit', 'Scoochie Bear', 'Kitten', 'Fluffy Muff', 'Squeaky Reminder'
 ];
 const emmyNames4 = [
-  'Smitten Kitten', 'Baby Fat', 'Marshmallow Fluff', 'Squeak Toy Joy', 'The 19-Toed One', 'Emmycat'
+  'Smitten Kitten', 'Baby Fat', 'Marshmellow Fluff', 'Squeak Toy Joy', 'The 19-Toed One', 'Emmycat'
 ];
 const emmyNames5 = [
   'Squeakers', 'Kitten Mitten', 'Bubbie Schmubs', 'Fatticus Catticus', 'Baby Girl', 'Fluff'
@@ -56,9 +56,11 @@ function displayDirection(e) {
 
 // Function to play game
 let gameActive = true;
+guess1.value = '';
+guess2.value = '';
+guess3.value = '';
 async function playGame() {
-  counter = 0;                                           /* TODO */
-  pickNameAndGroup(); winner = 'Fluff';                   /* PUT BACK */
+  winner = pickNameAndGroup();
   let showPlace = document.getElementById('games');
   let randomName;
   let showAName;
@@ -93,7 +95,7 @@ async function playGame() {
       showAName.style.right = '10%';
     }
     if (!verticle) {
-      showAName.style.bottom = '83%';
+      showAName.style.bottom = '75%';
       right ? showAName.style.right = '4%' : showAName.style.right = '75%';
     }
     showPlace.appendChild(showAName);
@@ -104,16 +106,12 @@ async function playGame() {
         bottom ? showAName.style.bottom = '95%' : showAName.style.bottom = '7%';
       }
       if (!verticle) {
-        right ? showAName.style.right = '37%' : showAName.style.right = '2%';
+        right ? showAName.style.right = '35%' : showAName.style.right = '2%';
       }
       showAName.style.transform = 'rotate(5deg)';
     }, 20)
     await delay(1180);
     showPlace.removeChild(showPlace.lastElementChild);
-    counter ++;                          /* TODO */
-    if (counter === 2) {             /* TODO */
-      gameActive = false;
-    }
   }
 }
 
@@ -122,7 +120,7 @@ playGame();
 
 // Function to check for winning guess
 const checkForWinner = (guess) => {
-  return guess.toLowerCase() === winner.toLowerCase() ? true : false;
+  return guess.toLowerCase().trim() === winner.toLowerCase() ? true : false;
 }
 
 // Function to inform of a winning guess
@@ -132,42 +130,128 @@ const showWinner = () => {
   winnerMessage.innerHTML = "Yes! You got it! <br> <span>YOU WIN!</span> <br> Congratulations!";
   winnerMessage.style.fontSize = '1.25rem';
   winnerMessage.style.color = 'black';
+  winnerMessage.style.lineHeight = '2rem';
+  winnerMessage.style.paddingTop = '1rem';
   messagePlace.appendChild(winnerMessage);
   let innerWinner = winnerMessage.querySelector('span');
   innerWinner.style.fontSize = '1.5rem';
   innerWinner.style.color = 'white';
   innerWinner.style.textShadow = '1px 1px 1px black';
+  innerWinner.style.paddingLeft = '1rem';
 }
 
-// Submit guess
+// Submit guess - Event handler function
 const enterBtn = (e) => {
-  if (e.target.id === 'btn1') {
+  if (e.target.id === 'btn1' || e.target.id === 'guess1') {
     if (checkForWinner(guess1.value)) {
       gameActive = false;
+      document.getElementById('btn1').removeEventListener('click', checkEntry);
+      document.getElementById('guess1').removeEventListener('keydown', enterKey);
       document.getElementById('guess1').style.backgroundColor = ('rgb(204, 230, 204)');
-      document.getElementById('btn1').removeEventListener('click', enterBtn);
+      document.getElementById('btn1').focus();
       showWinner();
     }
     else {
-      document.getElementById('btn1').removeEventListener('click', enterBtn);
+      document.getElementById('btn1').removeEventListener('click', checkEntry);
+      document.getElementById('guess1').removeEventListener('keydown', enterKey);
       document.getElementById('btn1').style.backgroundColor = ('rgb(169, 169, 169)');
       document.getElementById('guess1').style.backgroundColor = ('rgb(255, 204, 204)');
       let firstNoMessage = document.createElement('p');
       firstNoMessage.innerHTML = "Nope. That's not it.";
       messagePlace.appendChild(firstNoMessage);
-      setTimeout(() => {
+      messagePlace.appendChild(label2);
       document.getElementById('label2').style.display = 'block';
+      messagePlace.appendChild(guess2);
       document.getElementById('guess2').style.display = 'inline-block';
+      document.getElementById('guess2').focus();
+      messagePlace.appendChild(btn2);
       document.getElementById('btn2').style.display = 'inline-block';
-      }, 1200)
+    }
+  }
+  if (e.target.id === 'btn2' || e.target.id === 'guess2') {
+    if (checkForWinner(guess2.value)) {
+      gameActive = false;
+      document.getElementById('btn2').removeEventListener('click', checkEntry);
+      document.getElementById('guess2').removeEventListener('keydown', enterKey);
+      document.getElementById('guess2').style.backgroundColor = ('rgb(204, 230, 204)');
+      document.getElementById('btn2').focus();
+      showWinner();
+    }
+    else {
+      document.getElementById('btn2').removeEventListener('click', checkEntry);
+      document.getElementById('guess2').removeEventListener('keydown', enterKey);
+      document.getElementById('btn2').style.backgroundColor = ('rgb(169, 169, 169)');
+      document.getElementById('guess2').style.backgroundColor = ('rgb(255, 204, 204)');
+      let secondNoMessage = document.createElement('p');
+      secondNoMessage.innerHTML = "No. Sorry.";
+      messagePlace.appendChild(secondNoMessage);
+      messagePlace.appendChild(label3);
+      document.getElementById('label3').style.display = 'block';
+      messagePlace.appendChild(guess3);
+      document.getElementById('guess3').style.display = 'inline-block';
+      document.getElementById('guess3').focus();
+      messagePlace.appendChild(btn3);
+      document.getElementById('btn3').style.display = 'inline-block';
+    }
+  }
+  if (e.target.id === 'btn3' || e.target.id === 'guess3') {
+    if (checkForWinner(guess3.value)) {
+      gameActive = false;
+      document.getElementById('btn3').removeEventListener('click', checkEntry);
+      document.getElementById('guess3').removeEventListener('keydown', enterKey);
+      document.getElementById('guess3').style.backgroundColor = ('rgb(204, 230, 204)');
+      document.getElementById('btn3').focus();
+      showWinner();
+    }
+    else {
+      gameActive = false;
+      document.getElementById('btn3').removeEventListener('click', checkEntry);
+      document.getElementById('guess3').removeEventListener('keydown', enterKey);
+      document.getElementById('btn3').style.backgroundColor = ('rgb(169, 169, 169)');
+      document.getElementById('guess3').style.backgroundColor = ('rgb(255, 204, 204)');
+      let thirdNoMessage = document.createElement('p');
+      thirdNoMessage.innerHTML = "Awww. No, not this time.";
+      messagePlace.appendChild(thirdNoMessage);
+      messagePlace.appendChild(setupAnswer);
+      document.getElementById('setupAnswer').style.display = 'block';
+      let missedAnswer = document.createElement('p');
+      missedAnswer.id = "answer";
+      missedAnswer.innerHTML = winner;
+      messagePlace.appendChild(missedAnswer);
+    }
+  }
+}
+
+// Check function to see if guess is a valid attempt
+function checkEntry(e) {
+  if (e.target.id === 'btn1' || e.target.id === 'guess1') {
+    if (chosenGroup.some((nn) => nn.toLowerCase() === guess1.value.toLowerCase().trim())) {
+      enterBtn(e);
+    }
+  }
+  if (e.target.id === 'btn2' || e.target.id === 'guess2') {
+    if (chosenGroup.some((nn) => nn.toLowerCase() === guess2.value.toLowerCase().trim())) {
+      enterBtn(e);
+    }
+  }
+  if (e.target.id === 'btn3' || e.target.id === 'guess3') {
+    if (chosenGroup.some((nn) => nn.toLowerCase() === guess3.value.toLowerCase().trim())) {
+      enterBtn(e);
     }
   }
 }
 
 // Register event listeners for clicking the Enter buttons
-document.getElementById('btn1').addEventListener('click', enterBtn);
-document.getElementById('btn2').addEventListener('click', enterBtn);
-document.getElementById('btn3').addEventListener('click', enterBtn);
+document.getElementById('btn1').addEventListener('click', checkEntry);
+document.getElementById('btn2').addEventListener('click', checkEntry);
+document.getElementById('btn3').addEventListener('click', checkEntry);
 
-// Register event listener for pressing KEYS
-document.getElementById('goToMain').addEventListener('click', enterKey);
+// Register event listener for pressing the Enter key
+const enterKey = (e) => {
+  if (e.key === 'Enter') {
+    checkEntry(e);
+  }
+}
+document.getElementById('guess1').addEventListener('keydown', enterKey);
+document.getElementById('guess2').addEventListener('keydown', enterKey);
+document.getElementById('guess3').addEventListener('keydown', enterKey);
